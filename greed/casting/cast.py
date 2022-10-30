73793da7fc021f09261a8d04a2d7ead5a523ae25
Brandon Smith <smi11037@byui.edu>
2022-10-30 14:23:44 -0600

    Made changes to the cast class and added logic to remove objects that have fallen off screen and changed get first actor to only return player and score.

--- greed/casting/cast.py
+++ greed/casting/cast.py
@@ -1,3 +1,7 @@
+from ..shared.color import Color
+from ..casting.objects import Objects
+from ..shared.point import Point
+
 class Cast:
     """A collection of actors.
 
@@ -46,12 +50,24 @@ class Cast:
             List: All of the actors in the cast.
         """
         results = []
+        templist = []
+
         for group in self._actors:
+            #Check actors position to see if they have reached the end of the y axis and remove items off page
+            if(group == "gem" or  group=="stone"):
+                templist.extend(self._actors[group])
+                for item in templist:
+                    p = item.get_position()
+                    y = p.get_y()
+                    if y > 600:
+                        self.remove_actor(self, item)
             results.extend(self._actors[group])
+
+
         return results
 
     def get_first_actor(self, group):
-        """Gets the first actor in the given group.
+        """Gets the first actor in the given group but only returns player or score.
         
         Args:
             group (string): The name of the group.
@@ -60,16 +76,33 @@ class Cast:
             List: The first actor in the group.
         """
         result = None
-        if group in self._actors.keys():
-            result = self._actors[group][0]
+        if(group == 'score'):
+            if group in self._actors.keys():
+                result = self._actors[group][0]
+        elif(group == 'player'):
+            if group in self._actors.keys():
+                result = self._actors[group][0]
         return result
 
-    def remove_actor(self, group, actor):
+    def remove_actor(self, actor):
         """Removes an actor from the given group.
         
         Args:
             group (string): The name of the group.
             actor (Actor): The actor to remove.
         """
-        if group in self._actors:
-            self._actors[group].remove(actor)
+        '''If actor group is gem or stone'''
+        if((actor.group == "gem") or (actor.group == "stone")):
+            p = actor.position.get_position()
+            y = p.get_y()
+            if y < 600:
+                '''Create a blank Object to replace it if not at the end of the screen'''
+                blank_object = Objects()
+                blank_object.set_text(chr(' '))
+                blank_object.set_font_size(15)
+                blank_object.set_color(Color(0, 0, 0))
+                blank_object.set_position(actor.position)
+                self.add_actor("blank", blank_object)
+                self._actors[actor.group].add(blank_object)
+            '''Then delete the initial object'''
+            self._actors[actor.group].remove(actor)
