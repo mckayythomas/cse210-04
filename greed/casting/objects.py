63112c0b9d7b92ec4b16210612c0f3c999aeea26
mckayythomas <mckayycollins@gmail.com>
2022-11-03 12:26:47 -0500

    Created and changed the objects

--- greed/casting/objects.py
+++ greed/casting/objects.py
@@ -3,18 +3,24 @@ from .actor import Actor
 
 class Objects(Actor):
     """
-    An item of cultural or historical interest. 
+    A rock or gem
     
-    The responsibility of an Artifact is to provide a message about itself.
+    The responsibility of an Object is to provide points depending if it is a rock or a gem.
 
     Attributes:
-        _message (string): A short description about the artifact.
+        self._point (Int) a set of points depending on if it is a rock or a gem
+
     """
     def __init__(self):
         super().__init__()
-        self._message = ""
+        self._point = 0
 
-    def rock_gem(self):
+    def rock_gem(self, group):
+        if group == "gems":
+            self._point += 1
+        elif group == "stones":
+            self._point -= 1
+
         return self._point
     
     def get_item_type(self):
